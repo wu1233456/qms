07327bca808014e821faf0e89a5fbb494135da82
wuchunlin <1339472610@qq.com>
2021-03-31 21:07:35 +0800

    修改

--- sh/vpc_lab.sh
+++ sh/vpc_lab.sh
@@ -3,7 +3,7 @@
 ############################
 #npm instal
 ############################
-cd /www/site/com.lab/qms
+cd /www/site/com.lab/qmx
 npm install --unsafe-perm
 npm run prod
 
@@ -17,7 +17,7 @@ service openresty start;
 ############################
 # pip install
 ############################
-cd /www/site/com.lab/qms
+cd /www/site/com.lab/qmx
 
 apt-get install python3-pip
 pip3 install -r requirements.txt
@@ -25,9 +25,9 @@ pip3 install -r requirements.txt
 ############################
 # uwsgi
 ############################
-cd /www/site/com.lab/qms
+cd /www/site/com.lab/qmx
 if [[ ! -L /etc/uwsgi-emperor/vassals/flask_index.ini ]] && [[ ! -f /etc/uwsgi-emperor/vassals/flask_index.ini ]];then
-    ln -s /www/site/com.lab/qms/uwsgi/vpc_lab/flask_index.ini  /etc/uwsgi-emperor/vassals/flask_index.ini
+    ln -s /www/site/com.lab/qmx/uwsgi/vpc_lab/flask_index.ini  /etc/uwsgi-emperor/vassals/flask_index.ini
 fi;
 
 service uwsgi-emperor stop
@@ -38,7 +38,7 @@ service uwsgi-emperor status
 ############################
 #给予download文件夹全部权限
 ############################
-cd /www/site/com.lab/qms
+cd /www/site/com.lab/qmx
 chmod -R 777 download
 
 echo "Success"
